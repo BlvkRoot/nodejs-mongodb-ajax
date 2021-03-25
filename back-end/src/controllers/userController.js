32ce70984cf481319b161de095390964696dc033
blvkroot <hsalukamba17@gmail.com>
2021-03-25 23:52:13 +0100

    Check if user already exists.

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -4,19 +4,24 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
+    const { email } = req.body;
     console.log('====================================');
     console.log(req.body);
     console.log('====================================');
     try {
 
+        if (await User.findOne({email})) {
+            return res.send({ 'Error': 'User already exists.' });
+        }
+
         const user = await User.create(req.body);
 
-        return res.send({user});
+        return res.send({ user });
 
     } catch (error) {
-        return res.send({'Error': error});
+        return res.send({ 'Error': 'User not registered. Please try again' });
     }
-    
+
 });
 router.get('/show', async (req, res) => {
     return res.send('Show all users');
