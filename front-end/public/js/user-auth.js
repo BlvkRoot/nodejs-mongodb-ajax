8fdc8a83a855c6b7fead953b4c163292870a5048
BlvkRoot <hsalukamba17@gmail.com>
2021-03-25 16:14:14 +0000

    Toggle register and signIn

--- front-end/public/js/user-auth.js
+++ front-end/public/js/user-auth.js
@@ -1,5 +1,16 @@
 $(function () {
-    $('form.register').on('submit', function (e) {
+    const authSwitchLinks = document.querySelectorAll('.switch');
+    const authModals = document.querySelectorAll('.auth .modal');
+    const authWrapper = document.querySelector('.auth');
+
+    // toggle auth modals
+    authSwitchLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        authModals.forEach(modal => modal.classList.toggle('active'));
+    });
+    });
+    
+    $('form#register').on('submit', function (e) {
         event.preventDefault();
 
         console.log('Working...');
