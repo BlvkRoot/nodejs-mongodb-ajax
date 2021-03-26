4766674c9065e49522815b4799861256451153df
BlvkRoot <hsalukamba17@gmail.com>
2021-03-26 13:46:19 +0000

    Login User and disable register/login btn when clicked.

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,36 +1,58 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.post('/create', async (req, res) => {
-    const { email } = req.body;
-    console.log('====================================');
-    console.log(req.body);
-    console.log('====================================');
-    try {
-
-        if (await User.findOne({email})) {
-            return res.send({ 'Error': 'User already exists.' });
-        }
-
-        const user = await User.create(req.body);
-
-        return res.send({ user });
-
-    } catch (error) {
-        return res.send({ 'Error': 'User not registered. Please try again' });
+router.post("/create", async (req, res) => {
+  const { email } = req.body;
+  console.log("====================================");
+  console.log(req.body);
+  console.log("====================================");
+  try {
+    if (await User.findOne({ email })) {
+      return res.send({ Error: "User already exists." });
     }
 
-});
-router.get('/show', async (req, res) => {
-    return res.send('Show all users');
-});
-router.put('/update', async (req, res) => {
-    return res.send('User Updated');
-});
-router.delete('/delete', async (req, res) => {
-    return res.send('User Deleted');
+    const user = await User.create(req.body);
+
+    return res.send({ Success: 'Registration Successful.' });
+  } catch (error) {
+    return res.send({ Error: "User not registered, please try again" });
+  }
 });
 
-module.exports = app => app.use('/auth', router);
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log("====================================");
+  console.log(req.body);
+  console.log("====================================");
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      if (user.password === password) {
+        return res.send({ Success: "Login successful." });
+      } else {
+        return res.send({ Error: "Incorrect Password, please try again." });
+      }
+    } else {
+      return res.send({ Error: "User not found, please register." });
+    }
+  } catch (error) {
+    return res.send({ Error: "Login failed. Please try again" });
+  }
+
+});
+
+router.get("/show", async (req, res) => {
+  return res.send("Show all users");
+});
+router.put("/update", async (req, res) => {
+  return res.send("User Updated");
+});
+router.delete("/delete", async (req, res) => {
+  return res.send("User Deleted");
+});
+
+module.exports = (app) => app.use("/auth", router);
--- front-end/public/js/user-auth.js
+++ front-end/public/js/user-auth.js
@@ -4,6 +4,9 @@ $(function () {
     const authWrapper = document.querySelector('.auth');
     const apiBaseUrl = 'http://localhost:8000/';
     const errorDiv = $('.error');
+    const registerForm = $('form#register');
+    const loginForm = $('form#login');
+
 
     // toggle auth modals
     authSwitchLinks.forEach(link => {
@@ -12,32 +15,99 @@ $(function () {
         });
     });
 
-    $('form#register').on('submit', function (e) {
-        event.preventDefault();
+    const register = () => {
+        registerForm.on('submit', function (e) {
+            e.preventDefault();
+            const registerBtn = e.target[2];
+            registerBtn.disabled = true;
+    
+            const userData = {
+                "email": $('#email-register').val(),
+                "password": $('#password-register').val()
+            };
+    
+            $.ajax({
+                type: "POST",
+                url: `${apiBaseUrl}auth/create`,
+                data: userData,
+                dataType: "json",
+                success: function (response) {
+                    console.log('success: ', response);
+    
+                    if(response.Error)
+                    {
+                        errorDiv.addClass('show');
+                        errorDiv.text(response.Error);
 
-        const userData = {
-            "email": $('#email').val(),
-            "password": $('#password').val()
-        };
+                        toggleDisable(registerBtn);
+                        // Hide error Div after 10 secs
+                        hideErrorDiv();
 
-        $.ajax({
-            type: "POST",
-            url: `${apiBaseUrl}auth/create`,
-            data: userData,
-            dataType: "json",
-            success: function (response) {
-                console.log('success: ', response);
-
-                if(response.Error)
-                {
-                    errorDiv.addClass('show');
-                    errorDiv.text(response.Error);
-                }else {
-                    errorDiv.removeClass('show');
-                }
-            },
-            error: function (response) { console.log('error: ', response); }
+                    }else {
+                        errorDiv.removeClass('show'); 
+                    }
+                },
+                error: function (response) { console.log('error: ', response); }
+            });
+    
         });
+    }
 
-    });
+    const login = () => {
+        loginForm.on('submit', function (e) {
+            e.preventDefault();
+            const loginBtn = e.target[2];
+            loginBtn.disabled = true;
+    
+            const userData = {
+                "email": $('#email-login').val(),
+                "password": $('#password-login').val()
+            };
+    
+            $.ajax({
+                type: "POST",
+                url: `${apiBaseUrl}auth/login`,
+                data: userData,
+                dataType: "json",
+                success: function (response) {
+                    console.log('success: ', response);
+    
+                    if(response.Error)
+                    {
+                        errorDiv.addClass('show');
+                        errorDiv.text(response.Error);
+                        
+                        toggleDisable(loginBtn);
+
+                        // Hide error Div after 10 secs
+                        hideErrorDiv();
+                        
+                    }else {
+                        errorDiv.removeClass('show'); 
+                    }
+                },
+                error: function (response) { console.log('error: ', response); }
+            });
+    
+        });
+    }
+
+    const hideErrorDiv = () => {
+
+        setTimeout(() => {
+            errorDiv.removeClass('show');
+            errorDiv.text('');
+        }, 10000);
+    }
+
+    const toggleDisable = (btn) => {
+        setTimeout(() => {
+            btn.disabled = false;
+        }, 1800);
+    }
+
+    register();
+    login();
+
+
 });
